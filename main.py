55f0470aa6d0223253c7babd1e29a5cf694b60a2
chudo270 <1111-f11@mail.ru>
2025-04-18 12:22:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -264,9 +264,4 @@ async def main():
     await site.start()
 
     print(f"Webhook запущен на {webhook_url}")
-    import os
-    import uvicorn
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    
